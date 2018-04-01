81f012c26d3775e02ed24e0ddd5cc82033886596
Ryan Davidson <daviryan@aol.com>
2018-04-01 23:49:02 +0100

    Add support for publish_count for X retries to worker

--- lib/rabbit_mq/worker.rb
+++ lib/rabbit_mq/worker.rb
@@ -2,10 +2,13 @@
 
 module RabbitMQ
   class Worker
-    attr_reader :logger, :observers
+    attr_reader :logger, :parser, :observers, :max_retries
 
-    def initialize(logger)
+    def initialize(logger, parser, max_retries = 5)
       @logger = logger
+      @parser = parser
+      raise 'max_retries cannot be less than 1' if max_retries < 1
+      @max_retries = max_retries
       @observers = []
     end
 
@@ -35,51 +38,60 @@ module RabbitMQ
     end
 
     def run
-      RabbitMQ::Sinbin.setup(logger: logger, max_retries: 6, sinbin_ttl: 1000)
       channel.prefetch(1)
-      puts ' [*] Waiting for messages. To exit press CTRL+C'
+      logger.info ' [*] Waiting for messages. To exit press CTRL+C'
 
       begin
         queue
           .subscribe(manual_ack: true, block: true) do |delivery_info, properties, payload|
           logger.info " [x] Received message"
+          logger.info " [x] Parsing payload"
+          payload = parser.parse(payload)
           logger.info ' [x] Notifying observers'
           notify_observers(delivery_info, properties, payload)
           logger.info ' [x] Done, ack message'
           channel.ack(delivery_info.delivery_tag)
         rescue StandardError => error
-          logger.error "#{self.class.name} -> #{error.class}: #{error.message}" \
-            'Publishing to retry exchange.'
-          RabbitMQ::Publisher.retry_exchange(payload)
+          handle_error(error, delivery_info, properties, payload)
         end
       rescue Interrupt => _
         RabbitMQ::Connection.close
       end
     end
 
-    def retry_or_dead(payload)
-      properties = increment_message_count(payload)
-      logger.info "#{self.class.name}: delivery_count = #{payload[:delivery_count]} for #{body}"
+    def handle_error(error, delivery_info, _properties, payload)
+      logger.error "#{self.class.name}: #{error.class} #{error.message || ''}"
+      logger.debug "#{error.backtrace.join("\n\t")}"
+      if error.is_a? RabbitMQ::MessageError
+        logger.info "#{self.class.name} RabbitMQ::MessageError publishing to dead queue: #{payload}"
+        channel.nack(delivery_info.delivery_tag)
+        return
+      end
+      increment_message_count(payload)
       if exceeded_max_retries?(payload)
-        logger.info "#{self.class.name}: delivery_count exceeded max_retries of #{max_retries}" \
-          'publishing to dead queue'
-        RabbitMQ::Publisher.dead(payload)
+        logger.info "#{self.class.name}:" \
+          " delivery_count exceeded max_retries of #{max_retries};" \
+          ' publishing to dead queue'
+        channel.nack(delivery_info.delivery_tag)
       else
-        logger.info "#{self.class.name}: publishing to retry for #{retry_ttl}"
-        RabbitMQ::Publisher.retry_exchange(payload)
+        logger.info "#{self.class.name}: publishing to retry exchange"
+        channel.ack(delivery_info.delivery_tag)
+        RabbitMQ::Publisher.retry(payload)
       end
     end
 
     def exceeded_max_retries?(payload)
-      payload[:delivery_count] > max_retries
+      payload["delivery_count"] >= max_retries
     end
 
     def increment_message_count(payload)
-      if payload.key?(:delivery_count)
-        payload[:delivery_count] += 1
+      if payload.key?("delivery_count")
+        payload["delivery_count"] += 1
       else
-        payload[:delivery_count] = 0
+        payload["delivery_count"] = 1
       end
+      logger.info "#{self.class.name}: " \
+        "delivery_count = #{payload["delivery_count"]} for #{payload}"
     end
   end
 end
