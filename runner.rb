e47821f436dc57cc13448076b3976752c5cf875a
Ryan Davidson <daviryan@aol.com>
2018-04-01 23:42:28 +0100

    Add json parser to runner

--- runner.rb
+++ runner.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 require_relative './lib/rabbit_mq'
+require_relative './lib/worker_observer'
 
 logger = RabbitMQ::LoggerFactory.build
-observer = RabbitMQ::Observer.new(logger)
-worker = RabbitMQ::Worker.new(logger)
+parser = RabbitMQ::JSONParser
+worker = RabbitMQ::Worker.new(logger, parser)
+observer = RabbitMQ::WorkerObserver.new(logger)
 worker.add_observer(observer)
 worker.run
