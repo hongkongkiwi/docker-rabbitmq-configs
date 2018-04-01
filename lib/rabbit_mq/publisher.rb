8e868d4c65f4a79a8da1fbddfd63474b5a216998
Ryan Davidson <daviryan@aol.com>
2018-04-01 23:47:06 +0100

    Refactor publisher to only publish to exchanges

--- lib/rabbit_mq/publisher.rb
+++ lib/rabbit_mq/publisher.rb
@@ -23,7 +23,7 @@ module RabbitMQ
         )
     end
 
-    def retry_exchange(payload, routing_key = 'test')
+    def retry(payload, routing_key = 'test')
       channel
         .fanout(
           'my-exchange.retry',
@@ -31,38 +31,6 @@ module RabbitMQ
         ).publish(
           format_payload(payload),
           routing_key: routing_key,
-          persistent: true
-        )
-    end
-
-    def retry(payload, sinbin_ttl = 5000)
-      channel
-        .queue('my-queue.retry',
-               durable: true,
-               arguments: {
-                 'x-dead-letter-exchange' => 'my-exchange',
-                 'x-message-ttl' => sinbin_ttl
-               })
-        .publish(
-          format_payload(payload),
-          persistent: true
-        )
-    end
-
-    def invalid(payload)
-      channel
-        .queue('my-queue.invalid', durable: true)
-        .publish(
-          format_payload(payload),
-          persistent: true
-        )
-    end
-
-    def dead(payload)
-      channel
-        .queue('my-queue.dead', durable: true)
-        .publish(
-          format_payload(payload),
           persistent: true
         )
     end
