c72ed5983cbf7f5369780726d11939f48eb3dccb
Ryan Davidson <daviryan@aol.com>
2018-04-01 13:47:11 +0100

    Refactor worker into more methods

--- lib/rabbit_mq/worker.rb
+++ lib/rabbit_mq/worker.rb
@@ -1,29 +1,49 @@
 # frozen_string_literal: true
 
 module RabbitMQ
-  module Worker
-    extend self
+  class Worker
+    attr_reader :observers
 
-    def run
-      channel = RabbitMQ::Connection.channel
-      queue =
-        channel
+    def initialize(logger)
+      @logger = logger
+      @observers = []
+    end
+
+    def add_observer(observer)
+      raise 'Observer must have an #update method' unless observer.respond_to?(:update)
+      @observers << observer
+    end
+
+    def notify_observers(delivery_info, properties, body)
+      observers.each do |observer|
+        observer.update(delivery_info, properties, body)
+      end
+   end
+
+    def channel
+      RabbitMQ::Connection.channel
+    end
+
+    def queue
+      channel
         .queue('my-queue',
                durable: true,
                arguments: {
                  'x-dead-letter-exchange' => 'my-exchange.dead',
                  'x-message-ttl' => 10_000
                })
+    end
 
+    def run
       channel.prefetch(1)
       puts ' [*] Waiting for messages. To exit press CTRL+C'
 
       begin
         queue
-          .subscribe(manual_ack: true, block: true) do |delivery_info, _properties, body|
+          .subscribe(manual_ack: true, block: true) do |delivery_info, properties, body|
           puts " [x] Received '#{body}'"
-          # imitate some work
-          sleep body.count('.').to_i
+          puts ' [x] Notifying observers'
+          notify_observers(delivery_info, properties, body)
           puts ' [x] Done'
           channel.ack(delivery_info.delivery_tag)
         end
