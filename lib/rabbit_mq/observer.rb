47fc105045eacc9e77d2a0b049b712d4e989c8a2
Ryan Davidson <daviryan@aol.com>
2018-04-01 13:45:58 +0100

    Update observer with log confirmation method

--- lib/rabbit_mq/observer.rb
+++ lib/rabbit_mq/observer.rb
@@ -2,14 +2,22 @@
 
 module RabbitMQ
   class Observer
-    attr_reader :logger
+    attr_reader :logger, :observer_name
 
-    def initilize(logger)
+    def initialize(logger, observer_name = 'RabbitMQ::Observer')
+      @observer_name = observer_name
       @logger = logger
     end
 
     def update(delivery_info, properties, body)
-      logger.info "Received message: #{delivery_info} #{properties} #{body}"
+      log_confirmation(delivery_info, properties, body)
+    end
+
+    def log_confirmation(delivery_info, properties, body)
+      logger.info "#{observer_name} Received message: " \
+                  "delivery_info: #{delivery_info}; " \
+                  "properties: #{properties}; " \
+                  "body: #{body}."
     end
   end
 end
