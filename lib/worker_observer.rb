439b74a383ff19922e0fdc05246dca08a62d2f89
Ryan Davidson <daviryan@aol.com>
2018-04-02 21:26:19 +0100

    Extract error into separate method in worker observer

--- lib/worker_observer.rb
+++ lib/worker_observer.rb
@@ -6,14 +6,12 @@ module RabbitMQ
   class WorkerObserver < RabbitMQ::Observer
     def update(delivery_info, properties, payload)
       super(delivery_info, properties, payload)
+      payload_error unless payload.key?('message')
+      logger.info "====! SUCCESS #{payload['message']} !===="
+    end
 
-      if payload.is_a?(Hash) && payload.key?('message')
-        logger.info "+-----------------------------------+
-                     | SUCCESS #{payload['message']}     |
-                     +-----------------------------------+"
-      else
-        raise "#{self.class.name}: payload must have a message"
-      end
+    def payload_error
+      raise "#{self.class.name}: payload must have a message"
     end
   end
 end
