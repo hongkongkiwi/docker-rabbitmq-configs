ce3a7df1585e670f53672dcda94dbd2a768958a0
Ryan Davidson <daviryan@aol.com>
2018-04-02 21:24:45 +0100

    Add Hash test to parser

--- lib/rabbit_mq/json_parser.rb
+++ lib/rabbit_mq/json_parser.rb
@@ -4,12 +4,14 @@ require 'json'
 
 module RabbitMQ
   module JSONParser
-    extend self
-
-    def parse(payload)
-      JSON.parse(payload)
-    rescue JSON::ParserError
-      raise RabbitMQ::MessageInvalidError, 'JSON::ParserError Payload is not a hash'
+    class << self
+      def parse(payload)
+        parsed_payload = JSON.parse(payload)
+        raise JSON::ParserError unless parsed_payload.is_a?(Hash)
+      rescue JSON::ParserError
+        raise RabbitMQ::MessageInvalidError,
+              'JSON::ParserError Payload is not a hash'
+      end
     end
   end
 end
