00e943a190053f46b09ff84c6f2132af529d6d5e
Ryan Davidson <daviryan@aol.com>
2018-04-02 23:40:36 +0100

    Amend logging message to debug level

--- lib/rabbit_mq/publisher.rb
+++ lib/rabbit_mq/publisher.rb
@@ -30,7 +30,8 @@ module RabbitMQ
       end
 
       def retry(payload, routing_key = 'test')
-        logger.info "#{self.class.name} publishing to retry exchange: #{payload}"
+        logger.debug self.class.name \
+          " publishing to retry exchange: #{payload}"
         channel.ack(delivery_info.delivery_tag)
         channel
           .fanout(
