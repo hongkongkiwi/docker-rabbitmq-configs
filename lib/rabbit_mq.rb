913c4a12d45a3addad4635f9dd137bbb85d45e1c
Ryan Davidson <daviryan@aol.com>
2018-04-02 23:29:24 +0100

    Update rabbit_mq file

--- lib/rabbit_mq.rb
+++ lib/rabbit_mq.rb
@@ -5,6 +5,8 @@ end
 
 require_relative './rabbit_mq/connection'
 require_relative './rabbit_mq/publisher'
+require_relative './rabbit_mq/simple_observable'
+require_relative './rabbit_mq/queue'
 require_relative './rabbit_mq/worker'
 require_relative './rabbit_mq/logger_factory'
 require_relative './rabbit_mq/observer'
