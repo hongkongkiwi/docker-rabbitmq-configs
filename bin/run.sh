e469afada7e1b50881e9ca9b3cfb42af5ef72c0b
Ryan Davidson <daviryan@aol.com>
2018-04-10 20:42:31 +0100

    Add git submodule update to run script

--- bin/run.sh
+++ bin/run.sh
@@ -6,4 +6,5 @@ SILENT=$1
 ./bin/remove.sh $SILENT
 ./bin/build.sh
 docker run -d -p 5672:5672 -p 15672:15672 --name rabbitmq-docker rabbitmq-docker
+git submodule update
 ../wait-for-rabbitmq-docker/wait-for-rabbitmq-docker.sh rabbitmq-docker
